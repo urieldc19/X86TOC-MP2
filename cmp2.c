00eb15fffb0ac199c9c7681b2e0a768bf7ccccfc
leebrien <lee_sandoval@dlsu.edu.ph>
2024-11-24 13:54:36 +0800

    Prompt Pixel Values

--- cmp2.c
+++ cmp2.c
@@ -18,6 +18,11 @@ int main () {
     uint8_t* inputImage = (uint8_t*)malloc(size * sizeof(uint8_t)); // Use uint8_t
     float* outputImage = (float*)malloc(size * sizeof(float));
 
+    printf("Enter the pixel values (0-255):\n");
+    for (int i = 0; i < size; i++) {
+        scanf("%hhu", &inputImage[i]); // %hhu is still correct for uint8_t
+    }
+
     //imgCvtGrayIntoFloat(inputImage, outputImage, size);
 
     // Sample values to test print
